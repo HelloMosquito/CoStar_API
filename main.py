c35b1d7b288165068b56378a4fba913a782a3ce1
HelloMosquito <34199629+HelloMosquito@users.noreply.github.com>
2018-06-14 00:21:12 -0400

    reformat the main.py

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-
-
-
 from pyquery import PyQuery as pq
 import file_operation
 import sys
@@ -8,6 +5,7 @@ import summary
 import id_add
 import re
 import logging
+import redisdb
 
 
 def log(func):
@@ -26,10 +24,6 @@ def main():
     console.setFormatter(logging.Formatter("%(message)s"))
     logging.getLogger().addHandler(console)
 
-
-
-
-
     folder_name = 'Offices'
     # folder_name = 'Multifamily'
     file_id = "020-1.html"
@@ -39,35 +33,30 @@ def main():
     logging.info(file_path)
 
     file = file_operation.file_read(file_path)
-
-
-
     doc = pq(file)
 
-    # build_doc = doc('[data-viewmodelname="propertyBuildingInformation"] .label-value-pair')
+    file_id = re.match(r".*(?=.htm)", file_id).group()
 
-    # print(build_doc)
-
-    ID = re.match(r".*(?=.htm)", file_id).group()
-
-    s = summary.Summary(doc, ID)
-
+    s = summary.Summary(doc, file_id)
     a = s.assessment()
+
     logging.info('=====>')
-    # logging.info("file ID is:", ID)
-    logging.info("file ID is: {}".format(ID))
+    # logging.info("file file_id is:", file_id)
+    logging.info("file_id is: {}".format(file_id))
     # logging.info("main.py type(a) ->", type(a))
     logging.info("main.py type(a) -> {}".format(type(a)))
     # logging.info(dict(a))
     logging.info("{}".format(dict(a)))
     logging.info('=====>')
-    # logging.info("file ID is:", ID)
-    logging.info("file ID is: {}".format(ID))
+    # logging.info("file_id is:", file_id)
+    logging.info("file_id is: {}".format(file_id))
     logging.info('main.py result --->')
     # logging.info(s.result)
     # logging.info("{}".format(s.result).encode('utf-8'))
     logging.info(s.result.__str__().encode('utf-8'))
 
+    # Save results in Redis Database.
+    redisdb.save_in_redis('Summary', s.result, 0)
 
 
 if __name__ == '__main__':
